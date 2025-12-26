0eede25edc00a7d60ad56352d1f4c8bed57ad825
Pranav Suri <pranavsuri@icloud.com>
2025-12-26 04:19:16 +0100

    Create sa.py

---
+++ app/infrastructure/db/sa.py
@@ -0,0 +1,54 @@
+# app/infrastructure/db/sa.py
+"""
+Small SQLAlchemy 2.0 "prelude" so domain model files don't repeatedly import
+a huge list of types/helpers from sqlalchemy/sqlalchemy.orm.
+"""
+
+from sqlalchemy import (
+    String,
+    Text,
+    Boolean,
+    Integer,
+    BigInteger,
+    Float,
+    Numeric,
+    Date,
+    DateTime,
+    TIMESTAMP,
+    JSON,
+    Enum as SAEnum,
+    ForeignKey,
+    UniqueConstraint,
+    CheckConstraint,
+    PrimaryKeyConstraint,
+    Index,
+    text,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+__all__ = [
+    # Column types
+    "String",
+    "Text",
+    "Boolean",
+    "Integer",
+    "BigInteger",
+    "Float",
+    "Numeric",
+    "Date",
+    "DateTime",
+    "TIMESTAMP",
+    "JSON",
+    # Constraints / indexes / helpers
+    "SAEnum",
+    "ForeignKey",
+    "UniqueConstraint",
+    "CheckConstraint",
+    "PrimaryKeyConstraint",
+    "Index",
+    "text",
+    # ORM
+    "Mapped",
+    "mapped_column",
+    "relationship",
+]
